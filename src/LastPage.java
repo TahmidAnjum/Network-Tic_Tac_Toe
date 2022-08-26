118c39c822b2619a91ecf6900aa90d6c302ec9ad
Anjum <tahmidanjum320@gmail.com>
2022-08-27 04:13:24 +0600

    first commit

---
+++ src/LastPage.java
@@ -0,0 +1,21 @@
+//import javax.xml.soap.Text;
+
+import javafx.scene.text.Text;
+
+public class LastPage {
+    private Main main;
+
+    private Game game;
+
+    public Text text;
+
+    void setMain(Main main){
+        this.main=main;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+
+}
